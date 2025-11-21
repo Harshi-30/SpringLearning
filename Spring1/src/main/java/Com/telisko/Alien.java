cc3d511fd283ab8e30134c6bf95b1cde8d0039e7
harshita <harshita2002karma@gmail.com>
2025-11-21 23:46:36 +0530

    Setter injection

--- Spring1/src/main/java/Com/telisko/Alien.java
+++ Spring1/src/main/java/Com/telisko/Alien.java
@@ -1,10 +1,21 @@
 package Com.telisko;
 
 public class Alien {
-    int age;
+    private int age;
+
     public Alien(){
         System.out.println("object is created");
     }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        System.out.println("Set age");
+        this.age = age;
+    }
+
     public void code(){
         System.out.println("Coding");
     }
--- Spring1/src/main/java/Com/telisko/App.java
+++ Spring1/src/main/java/Com/telisko/App.java
@@ -8,13 +8,11 @@ public class App
     public static void main( String[] args ){
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
         Alien obj = (Alien) context.getBean("alien");
-        obj.age = 23;
-        System.out.println(obj.age);
+
+        System.out.println(obj.getAge());
         obj.code();
 
-        Alien obj2 = (Alien) context.getBean("alien");
-        System.out.println(obj2.age);
-        obj2.code();
+
     }
 
 }
