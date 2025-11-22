f6135f4c6752cad25aa248f88cbb3a2080fe64cd
harshita <harshita2002karma@gmail.com>
2025-11-22 17:33:04 +0530

    Constructor injection

--- Spring1/src/main/java/Com/telisko/Alien.java
+++ Spring1/src/main/java/Com/telisko/Alien.java
@@ -2,9 +2,15 @@ package Com.telisko;
 
 public class Alien {
     private int age;
+    private Laptop lap;
 
     public Alien(){
         System.out.println("object is created");
+    }
+    public Alien(int age, Laptop lap1){
+        System.out.println("Constructor called ");
+        this.age = age;
+
     }
 
     public int getAge() {
@@ -16,7 +22,16 @@ public class Alien {
         this.age = age;
     }
 
+    public Laptop getLap() {
+        return lap;
+    }
+
+    public void setLap(Laptop lap) {
+        this.lap = lap;
+    }
+
     public void code(){
         System.out.println("Coding");
+        lap.compile();
     }
 }
---
+++ Spring1/src/main/java/Com/telisko/Laptop.java
@@ -0,0 +1,11 @@
+package Com.telisko;
+
+public class Laptop {
+    public Laptop(){
+        System.out.println("Laptop is created");
+    }
+    public void compile(){
+        System.out.println("Compile");
+    }
+
+}
