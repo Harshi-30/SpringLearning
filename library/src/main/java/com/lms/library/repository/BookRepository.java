87c2765d974e3d116d33a43690eb10dade928f00
Harshi-30 <165143794+Harshi-30@users.noreply.github.com>
2025-12-12 22:57:13 +0530

    Delete library directory

--- library/src/main/java/com/lms/library/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.lms.library.repository;
-
-import com.lms.library.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book,String> {
-}
--- library/src/main/java/com/lms/library/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lms.library.repository;
-
-import com.lms.library.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student,String> {
-}
